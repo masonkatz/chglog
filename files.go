0c7b7c052ac2dace2f69159d67a47363e2b89447
Mason Katz <Mason.Katz@SoftIron.com>
2023-03-09 12:48:56 -0800

    fix: temp fix for upstream yaml bug

--- files.go
+++ files.go
@@ -26,8 +26,27 @@ func Parse(file string) (entries ChangeLogEntries, err error) {
 }
 
 // Save save ChangeLogEntries to a yml file.
-func (c *ChangeLogEntries) Save(file string) (err error) {
-	data, _ := yaml.Marshal(c)
-	// nolint: gosec,gocritic
-	return os.WriteFile(file, data, 0o644)
+func (c *ChangeLogEntries) Save(filename string) (err error) {
+	fout, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+
+	defer fout.Close()
+
+	// Work around for https://github.com/go-yaml/yaml/issues/643 (pending fix
+	// https://github.com/go-yaml/yaml/pull/864).
+	//
+	// YAML indentation is broken when parsing a signed commit with a signed
+	// annotated tag. The block is indented 4 spaces, but the second signature
+	// starts with only 2. This is not a flaw in the original C libyaml, this
+	// was a change specific to the go implementation.
+	//
+	// Changing the default indentation to 2 hides the bug. The only way to do
+	// this is to switch from Marshal to using an Encoder.
+
+	e := yaml.NewEncoder(fout)
+	e.SetIndent(2)
+
+	return e.Encode(c)
 }
